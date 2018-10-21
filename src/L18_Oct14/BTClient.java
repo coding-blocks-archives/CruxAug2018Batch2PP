bda03a9da05cd6ec7743843f29e30a05bbecd07e
Garima Chhikara <garimachhikara128@gmail.com>
2018-10-21 22:17:06 +0530

    BST Code

--- src/L18_Oct14/BTClient.java
+++ src/L18_Oct14/BTClient.java
@@ -10,23 +10,19 @@ package L18_Oct14;
 public class BTClient {
 
 	// 10 true 20 true 40 false false true 50 false false true 30 false false
+
+	// 10 true 20 true 40 true 50 true 60 true 70 false false false false false true 80 false true 90 false true 100 false false true 30 false false
 	public static void main(String[] args) {
 
 		BinaryTree bt = new BinaryTree();
 		bt.display();
 		System.out.println(bt.size());
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(bt.diameter());
+		System.out.println(bt.diameter2());
+
+		System.out.println(bt.isBalanced());
+		bt.preorder();
+		bt.preorderI();
 
 	}
 
--- src/L18_Oct14/BinaryTree.java
+++ src/L18_Oct14/BinaryTree.java
@@ -1,5 +1,7 @@
 package L18_Oct14;
 
+import java.awt.HeadlessException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -61,6 +63,17 @@ public class BinaryTree {
 
 	}
 
+	public BinaryTree(int[] pre, int[] in) {
+
+		this.root = construct(pre, 0, pre.length - 1, in, 0, in.length - 1);
+	}
+
+	private Node construct(int[] pre, int plo, int phi, int[] in, int ilo, int ihi) {
+
+		
+		
+	}
+
 	public void display() {
 
 		System.out.println("------------------");
@@ -117,13 +130,37 @@ public class BinaryTree {
 	}
 
 	private int max(Node node) {
+
+		if (node == null) {
+			return Integer.MIN_VALUE;
+		}
+
+		int lm = max(node.left);
+		int rm = max(node.right);
+
+		return Math.max(node.data, Math.max(lm, rm));
+
 	}
 
-	public boolean find() {
-		return find(this.root);
+	public boolean find(int item) {
+		return find(this.root, item);
 	}
 
-	private boolean find(Node node) {
+	private boolean find(Node node, int item) {
+
+		if (node == null) {
+			return false;
+		}
+
+		if (node.data == item) {
+			return true;
+		}
+
+		boolean lf = find(node.left, item);
+		boolean rf = find(node.right, item);
+
+		return lf || rf;
+
 	}
 
 	public int ht() {
@@ -131,6 +168,214 @@ public class BinaryTree {
 	}
 
 	private int ht(Node node) {
+
+		if (node == null) {
+			return -1;
+		}
+
+		int lh = ht(node.left);
+		int rh = ht(node.right);
+
+		return Math.max(lh, rh) + 1;
+
+	}
+
+	// diameter : max distance possible b/w any two leaf nodes
+	public int diameter() {
+		return diameter(this.root);
+	}
+
+	private int diameter(Node node) {
+
+		if (node == null) {
+			return 0;
+		}
+
+		int ld = diameter(node.left);
+		int rd = diameter(node.right);
+		int sp = ht(node.left) + ht(node.right) + 2;
+
+		return Math.max(sp, Math.max(ld, rd));
+
+	}
+
+	private class DiaPiar {
+		int ht = -1;
+		int diameter = 0;
+	}
+
+	public int diameter2() {
+		return diameter2(this.root).diameter;
+	}
+
+	private DiaPiar diameter2(Node node) {
+
+		if (node == null) {
+			DiaPiar bp = new DiaPiar();
+			return bp;
+		}
+
+		DiaPiar ldp = diameter2(node.left);
+		DiaPiar rdp = diameter2(node.right);
+
+		DiaPiar np = new DiaPiar();
+		np.ht = Math.max(ldp.ht, rdp.ht) + 1;
+
+		int ld = ldp.diameter;
+		int rd = rdp.diameter;
+		int sp = ldp.ht + rdp.ht + 2;
+
+		np.diameter = Math.max(sp, Math.max(ld, rd));
+
+		return np;
+
+	}
+
+	private class BalPair {
+
+		int ht = -1;
+		boolean isBal = true;
+	}
+
+	public boolean isBalanced() {
+
+		return isBalanced(this.root).isBal;
+	}
+
+	private BalPair isBalanced(Node node) {
+
+		if (node == null) {
+			BalPair bp = new BalPair();
+			return bp;
+		}
+		BalPair lbp = isBalanced(node.left);
+		BalPair rbp = isBalanced(node.right);
+
+		int bf = Math.abs(lbp.ht - rbp.ht);
+
+		BalPair np = new BalPair();
+		np.ht = Math.max(lbp.ht, rbp.ht) + 1;
+
+		if (lbp.isBal && rbp.isBal && bf <= 1) {
+			np.isBal = true;
+		} else {
+			np.isBal = false;
+		}
+
+		return np;
+	}
+
+	public void preorder() {
+		preorder(this.root);
+		System.out.println();
+	}
+
+	private void preorder(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		// N
+		System.out.print(node.data + " ");
+
+		// L
+		preorder(node.left);
+
+		// R
+		preorder(node.right);
+
+	}
+
+	public void postorder() {
+		postorder(this.root);
+	}
+
+	private void postorder(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		// L
+		postorder(node.left);
+
+		// R
+		postorder(node.right);
+
+		// N
+		System.out.print(node.data);
+
+	}
+
+	public void inorder() {
+		inorder(this.root);
+	}
+
+	private void inorder(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		// L
+		inorder(node.left);
+
+		// N
+		System.out.print(node.data);
+
+		// R
+		inorder(node.right);
+
+	}
+
+	private class Pair {
+		Node node;
+		boolean selfDone;
+		boolean leftDone;
+		boolean rightDone;
+	}
+
+	public void preorderI() {
+
+		LinkedList<Pair> stack = new LinkedList<>();
+		Pair sp = new Pair();
+		sp.node = this.root;
+		stack.addFirst(sp);
+
+		while (!stack.isEmpty()) {
+
+			Pair tp = stack.getFirst();
+
+			if (tp.node == null) {
+				stack.removeFirst();
+				continue;
+			}
+
+			if (tp.selfDone == false) {
+				System.out.print(tp.node.data + " ");
+				tp.selfDone = true;
+			} else if (tp.leftDone == false) {
+				Pair np = new Pair();
+				np.node = tp.node.left;
+				stack.addFirst(np);
+
+				tp.leftDone = true;
+			} else if (tp.rightDone == false) {
+
+				Pair np = new Pair();
+				np.node = tp.node.right;
+				stack.addFirst(np);
+
+				tp.rightDone = true;
+			} else {
+				stack.removeFirst();
+			}
+
+		}
+
+		System.out.println();
+
 	}
 
 }
